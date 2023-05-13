fdb8fc8a042eeb8900210163c52a8a851b8df5c7
ge94jip <ge94jip@mytum.de>
2023-05-13 15:07:25 +0200

    jm

--- src/gad/binarysearch/BinSea.java
+++ src/gad/binarysearch/BinSea.java
@@ -30,28 +30,39 @@ public final class BinSea {
     }
 
     public static int search(int[] sortedData, int value, boolean lowerBound, Result result) {
-        int grenze = -1;
-        int index = search(sortedData,value,result);
+        int grenze = search(sortedData,value,result);
         if(lowerBound){
-            if (sortedData[index] < value) {
-                grenze = index + 1;
+            if (sortedData[grenze] >= value){
+                while (sortedData[grenze] >= value){
+                    grenze--;
+                }
             }
-            else {
-                grenze = index;
+            if (sortedData[grenze] < value){
+                while (sortedData[grenze] < value){
+                    grenze++;
+                    if (grenze > sortedData.length - 1){
+                        return -1;
+                    }
+                }
             }
+
         }
         else {
-            if (sortedData[index] > value) {
-                grenze = index - 1;
+            if (sortedData[grenze] <= value){
+                while (sortedData[grenze] <= value){
+                    grenze++;
+                }
             }
-            else {
-                grenze = index;
+            if (sortedData[grenze] > value){
+                while (sortedData[grenze] > value){
+                    grenze--;
+                    if (grenze < 0){
+                        return -1;
+                    }
+                }
             }
+
         }
-        if (grenze > sortedData.length){
-            grenze = -1;
-        }
-        result.addStep(grenze);
         return grenze;
     }
 
@@ -62,11 +73,11 @@ public final class BinSea {
     public static void main(String[] args) {
         int[] array = new int[] { 2,7,7,42,69,1337,2000,9001};
 
-        System.out.println(search(array, 7, new StudentResult()));
-        System.out.println(search(array, 100, new StudentResult()));
+       //System.out.println(search(array, 7, new StudentResult()));
+        //System.out.println(search(array, 100, new StudentResult()));
 
-        //System.out.println(search(array, 7, false, new StudentResult()));
-        //System.out.println(search(array, 100, true, new StudentResult()));
+        System.out.println(search(array, 7, false, new StudentResult()));
+        System.out.println(search(array, 100, true, new StudentResult()));
 
         //System.out.println(search(array, new NonEmptyInterval(7, 1500), new StudentResult(), new StudentResult()));
         //System.out.println(search(array, new NonEmptyInterval(9002, 10000), new StudentResult(), new StudentResult()));

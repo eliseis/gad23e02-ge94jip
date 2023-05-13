cdbf74013c2337c9b3b764b4416f2b336ffeb69b
ge94jip <ge94jip@mytum.de>
2023-05-13 13:52:26 +0200

    jm

--- src/gad/binarysearch/BinSea.java
+++ src/gad/binarysearch/BinSea.java
@@ -9,29 +9,52 @@ public final class BinSea {
 
     public static int search(int[] sortedData, int value, Result result) {
         int from = 0;
-        int to = sortedData.length;
+        int to = sortedData.length - 1;
         int index = -1;
         while (from <= to){
+            if (to - from <= 1){
+                break;
+            }
             int mid = from + (to - from) / 2;
+            result.addStep(mid);
             if (sortedData[mid] < value) {
-                from = mid - 1;
+                from = mid + 1;
                 index = from;
-                result.addStep(index);
             } else if (sortedData[mid] > value) {
-                to = mid - 1;
+                to = mid;
                 index = to;
-                result.addStep(index);
             } else if (sortedData[mid] == value) {
                 index = mid;
                 break;
             }
         }
-        result.addStep(index);
         return index;
     }
 
     public static int search(int[] sortedData, int value, boolean lowerBound, Result result) {
-        return 0;
+        int grenze = -1;
+        int index = search(sortedData,value,result);
+        if(lowerBound){
+            if (sortedData[index] < value) {
+                grenze = index + 1;
+            }
+            else {
+                grenze = index;
+            }
+        }
+        else {
+            if (sortedData[index] > value) {
+                grenze = index - 1;
+            }
+            else {
+                grenze = index;
+            }
+        }
+        if (grenze > sortedData.length){
+            grenze = -1;
+        }
+        result.addStep(grenze);
+        return grenze;
     }
 
     public static Interval search(int[] sortedData, NonEmptyInterval valueRange, Result resultLower, Result resultHigher) {
@@ -42,12 +65,12 @@ public final class BinSea {
         int[] array = new int[] { 2, 7, 7, 42, 69, 1337, 2000, 9001 };
 
         System.out.println(search(array, 7, new StudentResult()));
-        System.out.println(search(array, 100, new StudentResult()));
+        //System.out.println(search(array, 100, new StudentResult()));
 
-        System.out.println(search(array, 7, false, new StudentResult()));
-        System.out.println(search(array, 100, true, new StudentResult()));
+        //System.out.println(search(array, 7, false, new StudentResult()));
+        //System.out.println(search(array, 100, true, new StudentResult()));
 
-        System.out.println(search(array, new NonEmptyInterval(7, 1500), new StudentResult(), new StudentResult()));
-        System.out.println(search(array, new NonEmptyInterval(9002, 10000), new StudentResult(), new StudentResult()));
+        //System.out.println(search(array, new NonEmptyInterval(7, 1500), new StudentResult(), new StudentResult()));
+        //System.out.println(search(array, new NonEmptyInterval(9002, 10000), new StudentResult(), new StudentResult()));
     }
 }

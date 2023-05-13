f329b39afd608ebb00f6eb2395b5adf99a495cd5
ge94jip <ge94jip@mytum.de>
2023-05-13 14:37:44 +0200

    jm

--- src/gad/binarysearch/BinSea.java
+++ src/gad/binarysearch/BinSea.java
@@ -27,7 +27,7 @@ public final class BinSea {
             }
 
         }
-        while (from != to && from <= to){
+        while (from < to){
             int mid = from + (to - from) / 2;
             result.addStep(mid);
             if (sortedData[mid] < value) {
